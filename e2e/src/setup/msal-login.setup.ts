33fa6da8ed9d4733055c7dd73c78c4d6de15f41c
Andrew Alderson <andrew.alderson@outlook.com>
2026-01-18 06:45:25 +0000

    refactor: cache msal tokens with proper schema

--- e2e/src/setup/msal-login.setup.ts
+++ e2e/src/setup/msal-login.setup.ts
@@ -1,24 +1,41 @@
-import { Authority, ProtocolMode, ScopeSet } from '@azure/msal-common/browser';
 import { test as setup } from '@playwright/test';
-import { existsSync, mkdirSync, writeFileSync } from 'node:fs';
-import path from 'node:path';
+import { existsSync, mkdirSync, statSync, writeFileSync } from 'node:fs';
+import * as path from 'node:path';
 import { tryGetEnviromentVariable } from '../utils';
-import {
-  acquireTokenWithUsernameAndPassword,
-  createCachableAccessToken,
-  createCachableAccount,
-  createCachableIdToken,
-  createCachableRefreshToken,
-} from './token-helpers';
+
+function fileOlderThan(filePath: string, durationMs: number) {
+  try {
+    // Ensure file exists
+    if (!existsSync(filePath)) {
+      throw new Error(`File not found: ${filePath}`);
+    }
+
+    // Get file stats
+    const stats = statSync(filePath);
+
+    // Last modification time
+    const mtime = stats.mtime.getTime();
+
+    // Current time
+    const now = Date.now();
+
+    // Compare age
+    return now - mtime > durationMs;
+  } catch (err: any) {
+    console.error(`Error checking file age: ${err.message}`);
+    return false;
+  }
+}
 
 const sessionStorageFilePath = tryGetEnviromentVariable(
-  'SESSION_STORAGE_FILE_PATH'
+  'SESSION_STORAGE_FILE_PATH',
 );
 
+const hourInMilliseconds = 60 * 60 * 1000;
+
 setup.skip(
-  existsSync(
-    sessionStorageFilePath
-  ) /*&& !fileOlderThan(sessionStorageFilePath, "1h"*/
+  existsSync(sessionStorageFilePath) &&
+    !fileOlderThan(sessionStorageFilePath, hourInMilliseconds),
 );
 setup('msal-login', async ({ request, page }) => {
   const username = tryGetEnviromentVariable('MSAL_USERNAME');
@@ -31,88 +48,61 @@ setup('msal-login', async ({ request, page }) => {
   // as either an api scope or the client id is required
   const scopes = tryGetEnviromentVariable('MSAL_SCOPES', clientId);
 
-  const externalTokenResponse = await acquireTokenWithUsernameAndPassword(
-    request,
-    username,
-    password,
-    authority,
-    clientId,
-    scopes
-  );
+  const tokenRequest = await request.post(`${authority}/oauth2/v2.0/token`, {
+    form: {
+      grant_type: 'password',
+      client_id: clientId,
+      username,
+      password,
+      scope: `openid offline_access ${scopes}`,
+      response_type: 'token id_token',
+      client_info: 1, // this is undocumented but needs to be set to return client_info in the ExternalTokenResponse when caching the tokens
+    },
+  });
 
-  const authorityEntity = new Authority(
-    authority,
-    {} as any,
-    {} as any,
-    { protocolMode: ProtocolMode.AAD } as any,
-    {} as any,
-    ''
-  );
-  const tokens = [];
-  const tokenKeys: Record<string, string[]> = {};
+  const tokenResponse = await tokenRequest.json();
 
-  const account = createCachableAccount(externalTokenResponse, authorityEntity);
-  tokens.push({ ...account });
-  tokens.push({ key: 'msal.account.keys', value: [account.key] });
-
-  const { value: accountEntity } = account;
-
-  const idToken = createCachableIdToken(
-    externalTokenResponse,
-    accountEntity.homeAccountId,
-    accountEntity.environment,
-    clientId,
-    accountEntity.realm
-  );
-  if (idToken) {
-    tokens.push({ ...idToken });
-    tokenKeys['idToken'] = [idToken.key];
-  }
-
-  const accessToken = createCachableAccessToken(
-    externalTokenResponse,
-    accountEntity.homeAccountId,
-    accountEntity.environment,
-    accountEntity.realm,
-    clientId,
-    ScopeSet.fromString(scopes),
-    externalTokenResponse.expires_in || 0,
-    externalTokenResponse.ext_expires_in ||
-      externalTokenResponse.expires_in ||
-      0
-  );
-  if (accessToken) {
-    tokens.push({ ...accessToken });
-    tokenKeys['accessToken'] = [accessToken.key];
-  }
-
-  const refreshToken = createCachableRefreshToken(
-    externalTokenResponse,
-    accountEntity.homeAccountId,
-    accountEntity.environment,
-    clientId
-  );
-
-  if (refreshToken) {
-    tokens.push({ ...refreshToken });
-    tokenKeys['refreshToken'] = [refreshToken.key];
-  }
-
-  tokens.push({ key: `msal.token.keys.${clientId}`, value: tokenKeys });
-
-  // add this initScript before we naivigate so that the session storage is set
-  // preventing a redirect to the b2c endpoint
-  await page.addInitScript((tokens) => {
-    for (const { key, value } of tokens) {
-      window.sessionStorage.setItem(key, JSON.stringify(value));
-    }
-  }, tokens);
-
+  // IMPORTANT: Navigate before adding the script to the page below
+  // Failure to do this will result in the script executing in a non secure browser context
+  // causing calls in the msal browser library to window.crypto.subtle to fail
   await page.goto('/');
 
-  const sessionStorage: string = await page.evaluate(() =>
-    JSON.stringify(window.sessionStorage)
+  // Need to add the msal-browser library to the page so we can instantiate a PublicCLientApplication below
+  // We use the version from node_modules so there are no version mismatches and becuase new version
+  // of the library are no longer available on the Microsoft CDN
+  const moduleFile = path.resolve(
+    __dirname,
+    '../../../node_modules/@azure/msal-browser/lib/msal-browser.min.js',
   );
+
+  // Add the msal browser script to the page
+  await page.addScriptTag({
+    path: moduleFile,
+    type: 'module',
+  });
+
+  // Have the msal browser library add the tokens to browser storage
+  // Doing it this way will ensure that the browser storage keys have the correct schema
+  await page.evaluate(
+    async ({ tokenResponse, authority, clientId, scopes }) => {
+      const pca = await (
+        window as any
+      ).msal.PublicClientApplication.createPublicClientApplication({
+        auth: { authority, clientId },
+      });
+      await pca
+        .getTokenCache()
+        .loadExternalTokens({ authority, scopes }, tokenResponse, {
+          extendedExpiresOn: tokenResponse.expires_in,
+        });
+    },
+    { tokenResponse, authority, clientId, scopes },
+  );
+
+  const sessionStorage: string = await page.evaluate(() =>
+    JSON.stringify(window.sessionStorage),
+  );
+
   const dir = path.dirname(sessionStorageFilePath);
   if (!existsSync(dir)) {
     mkdirSync(dir, { recursive: true });
